10df01eac4533d1133417a07c24958f4814845e8
刘刈 <liuyi@udotek.com>
2018-07-17 17:12:33 +0800

    twosumII (with sorted list) done

--- TwoSum.py
+++
@@ -1,6 +0,0 @@
-class Solution:
-    def twoSum(self, nums, target):
-        for i in range(len(nums)):
-            for x in range(i+1,len(nums)):
-                if i!=x and target==nums[i]+nums[x]:
-                    return [i,x]
---
+++ TwoSumII.py
@@ -0,0 +1,28 @@
+'''
+num已排序
+'''
+class Solution:
+
+    def b_search(self, nums, target):
+        low = 0
+        high = len(nums) - 1
+        while low <= high:
+            mid = (low + high) // 2
+            if nums[mid] == target:
+                return mid
+            elif nums[mid] > target:
+                high = mid - 1
+            else:
+                low = mid + 1
+
+    def twoSum(self,nums,target):  # f返回[i,result]，
+        nums1 = nums.copy()
+        i=0
+        while i <= len(nums):  # 此处切片
+            nums2 = nums1[i+1:]
+            target1 = target - nums[i]
+            j = self.b_search(nums2,target1)
+            if j is None:
+                i+=1
+            else:
+                return [i,i+j+1]
